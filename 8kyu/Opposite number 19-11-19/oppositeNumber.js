b0e699b505935aa3a5fd518e1161531a47e40d58
cbastn <pro2j.lol@gmail.com>
2019-11-19 19:40:28 -0700

    Opposite number kata

---
+++ 8kyu/Opposite number 19-11-19/oppositeNumber.js
@@ -0,0 +1,19 @@
+/*
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+function opposite(number) {
+    //A number multiplied by -1 will change its signs 
+
+    return number * -1
+  }
+
+
+//Tests
+
+  console.log(opposite(1), -1,)
