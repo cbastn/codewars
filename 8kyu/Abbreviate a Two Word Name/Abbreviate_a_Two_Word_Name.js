46733b92d511ddf115e1b7b301f4eb13b014bc41
cbastn <pro2j.lol@gmail.com>
2020-05-19 06:48:42 -0600

    3 mroe code katas

---
+++ 8kyu/Abbreviate a Two Word Name/Abbreviate_a_Two_Word_Name.js
@@ -0,0 +1,21 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// Patrick Feeney => P.F
+
+function abbrevName(name){
+    // code away
+    name = name.split(' ');
+    return name[0][0].toUpperCase()+ '.' + name[1][0].toUpperCase();
+}
+
+console.log(abbrevName("Sam Harris"), "S.H");
+console.log(abbrevName("Patrick Feenan"), "P.F");
+console.log(abbrevName("Evan Cole"), "E.C");
+console.log(abbrevName("P Favuzzi"), "P.F");
+console.log(abbrevName("David Mendieta"), "D.M");
