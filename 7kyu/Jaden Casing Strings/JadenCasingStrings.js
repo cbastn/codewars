be8413b3d4f35d53dc74ea9c21f92628a7479e92
cbastn` <pro2j.lol@gmail.com>
2021-01-03 19:16:58 -0700

    katas 01-03-21, day 3/365

---
+++ 7kyu/Jaden Casing Strings/JadenCasingStrings.js
@@ -0,0 +1,8 @@
+//Jaden Casing Strings
+
+String.prototype.toJadenCase = function () {
+  //...
+};
+
+var str = "How can mirrors be real if our eyes aren't real";
+console.log(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
