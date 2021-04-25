365f199b2871c60fb21b098e3b69b85676e9c682
cbastn` <pro2j.lol@gmail.com>
2021-04-25 17:31:09 -0600

    friend or foe

---
+++ 7kyu/Friend or Foe/FriendorFoe.js
@@ -0,0 +1,22 @@
+// //Friend or Foe?
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+function friend(friends){
+    //your code here
+    return friends.reduce((arr, curr) =>{
+        curr.length === 4 ? arr.push(curr) : null
+        return arr
+    },[])
+}
+
+console.log(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"]);
+console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"]);
+console.log(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"]);
+console.log(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"]);
--- 8kyu/Get the mean of an array 04-25-21/getTheMeanOfAnArray.js
+++ 8kyu/Get the mean of an array 04-25-21/getTheMeanOfAnArray.js
@@ -17,4 +17,4 @@ function getAverage(marks){
  console.log(getAverage([2,2,2,2]),2);
  console.log(getAverage([1,2,3,4,5,]),3);
  console.log(getAverage([1,1,1,1,1,1,1,2]),1);
- 
+    
